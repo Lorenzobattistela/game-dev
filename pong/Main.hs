ba31e277a191e5d45dfe45de53a6b6e41316847b
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-07-10 09:11:16 -0300

    refactor: using let..in -> type defs -> using map to detect keyPressed

--- pong/Main.hs
+++ pong/Main.hs
@@ -1,16 +1,42 @@
 module Main where
 
+import qualified Data.Map as Map
+
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.Pure.Game
 
+type Radius = Float
+type Position = (Float, Float)
+type Velocity = (Float, Float) -- (x, y) where x is the horizontal velocity and y is the vertical
+
+data Bounce = BounceUp | BounceDown deriving Show
+
+data ControlKey = WKey | SKey | ArrUpKey | ArrDownKey deriving (Eq, Ord, Show)
+type Controls = Map.Map ControlKey Bool
+
+data PongGame = Game
+  { ballLoc :: Position -- x y location for pong ball
+  , ballVeloc :: Velocity -- pong ball velocity
+  , player1 :: Float -- left player paddle height where 0 is middle screen
+  , player2 :: Float -- right player paddle height
+  , paused :: Bool
+  , controls :: Controls
+  , seed :: Int
+  , nextBounceDirection :: Bounce
+  } deriving Show
+
+
 width, height :: Int
 paddleWidth, paddleInnerWidth, paddleHeight :: Float
-ballInitialVelocity :: (Float, Float)
+ballInitialVelocity :: Velocity 
 player1InitPosition, player2InitPosition :: Float
 paddleMargin :: Float
 bottomTopLineThickness :: Float
 ballRadius :: Float
+window :: Display
+background :: Color
+fps :: Int
 
 width = 600
 height = 600
@@ -23,22 +49,18 @@ player2InitPosition = 0
 paddleMargin = 25
 bottomTopLineThickness = 10
 ballRadius = 10
+window = InWindow "Pong" (width, height) (0, 0)
+background = black
+fps = 120
 
-data Bounce = BounceUp | BounceDown deriving Show
 
-data PongGame = Game
-  { ballLoc :: (Float, Float) -- x y location for pong ball
-  , ballVeloc :: (Float, Float) -- pong ball velocity
-  , player1 :: Float -- left player paddle height where 0 is middle screen
-  , player2 :: Float --right player paddle height
-  , paused :: Bool
-  , wKeyPressed :: Bool
-  , sKeyPressed :: Bool
-  , arrUpKeyPressed :: Bool
-  , arrDownKeyPressed :: Bool
-  , seed :: Int
-  , nextBounceDirection :: Bounce
-  } deriving Show
+initialControls :: Controls
+initialControls = Map.fromList
+  [ (WKey, False)
+  , (SKey, False)
+  , (ArrUpKey, False)
+  , (ArrDownKey, False)
+  ]
 
 initialState :: PongGame
 initialState = Game
@@ -47,13 +69,16 @@ initialState = Game
   , player1 = player1InitPosition
   , player2 = player2InitPosition 
   , paused = False
-  , wKeyPressed = False
-  , sKeyPressed = False
-  , arrUpKeyPressed = False
-  , arrDownKeyPressed = False
+  , controls = initialControls
   , seed = 0
   , nextBounceDirection = BounceUp
   }
+
+updateControl :: PongGame -> ControlKey -> Bool -> PongGame
+updateControl game key value = game { controls = Map.insert key value (controls game) }
+
+isKeyPressed :: PongGame -> ControlKey -> Bool
+isKeyPressed game key = Map.findWithDefault False key (controls game)
 
 
 pseudoRandom :: Int -> Bounce -> Int -> Int
@@ -62,66 +87,63 @@ pseudoRandom seed BounceDown limit = -(((1103515245 * seed + 12345) `mod` limit)
 
 render :: PongGame -> Picture
 render game =
-  pictures [ball, walls, makePaddle rose (fromIntegral (height `div` 2) - paddleMargin) $ player1 game, makePaddle orange (-(fromIntegral (height `div` 2) - paddleMargin )) $ player2 game]
-  where
-    ball = uncurry translate (ballLoc game) $ color ballColor $ circleSolid ballRadius
+  let
     ballColor = dark red
+    ball = uncurry translate (ballLoc game) $ color ballColor $ circleSolid ballRadius
 
+    wallColor = greyN 0.5
     wall :: Float -> Picture
     wall offset = 
       translate 0 offset $ color wallColor $ rectangleSolid (fromIntegral (width - 50)) bottomTopLineThickness
-
-    wallColor = greyN 0.5
     walls = pictures [wall (fromIntegral (height `div` 2)), wall (-(fromIntegral (height `div` 2)))]
 
+    paddleColor = light (light blue)
     makePaddle :: Color -> Float -> Float -> Picture
     makePaddle col x y = pictures
       [ translate x y $ color col $ rectangleSolid paddleWidth paddleHeight
-      , translate x y $ color paddleColor $ rectangleSolid (paddleInnerWidth) paddleHeight
+      , translate x y $ color paddleColor $ rectangleSolid paddleInnerWidth paddleHeight
       ]
-    paddleColor = light (light blue)
+  in
+    pictures [ball, walls, 
+              makePaddle rose (fromIntegral (height `div` 2) - paddleMargin) $ player1 game, 
+              makePaddle orange (-(fromIntegral (height `div` 2) - paddleMargin)) $ player2 game]
 
 moveBall :: Float -> PongGame -> PongGame
-moveBall seconds game = game { ballLoc = (x', y') }
-  where
-    (x, y) = ballLoc game
-    (vx, vy) = ballVeloc game
+moveBall seconds game =
+    let (x, y) = ballLoc game
+        (vx, vy) = ballVeloc game
+        x' = x + vx * seconds
+        y' = y + vy * seconds
+    in game { ballLoc = (x', y') }
 
-    x' = x + vx * seconds
-    y' = y + vy * seconds
 
-type Radius = Float
-type Position = (Float, Float)
+paddleBounce game = 
+    let (vx, vy) = ballVeloc game
+        bounceDirection = nextBounceDirection game
+        oldSeed = seed game
+        newSeed = oldSeed + 42
+        hadCollision = paddleCollision game
+        randomFac = pseudoRandom oldSeed bounceDirection 30
 
-paddleBounce game = game { ballVeloc = (vx', vy'), seed = newSeed, nextBounceDirection = nextBounce }
-  where
-    (vx, vy) = ballVeloc game
-    
-    bounceDirection = nextBounceDirection game
+        nextBounce = 
+          if odd randomFac
+          then BounceUp
+          else BounceDown
 
-    oldSeed = seed game
-    newSeed = oldSeed + 42
-
-    hadCollision = paddleCollision game
-
-    randomFac = pseudoRandom oldSeed bounceDirection 30
-
-    nextBounce = if odd randomFac
-                then BounceUp
-                else BounceDown
- 
-    vx' = if paddleCollision game
+        vx' =
+          if paddleCollision game
           then -(vx + fromIntegral randomFac)
           else vx
 
-    vy' = if paddleCollision game
+        vy' = 
+          if paddleCollision game
           then vy + fromIntegral randomFac
           else vy
-
+    in game { ballVeloc = (vx', vy'), seed = newSeed, nextBounceDirection = nextBounce }
 
 paddleCollision :: PongGame -> Bool
-paddleCollision game = leftPaddleCollision || rightPaddleCollision
-  where
+paddleCollision game =
+  let
     (x, y) = ballLoc game
     leftPaddleX = -fromIntegral (width `div` 2) + paddleWidth
     rightPaddleX = fromIntegral (width `div` 2) - paddleWidth
@@ -137,23 +159,25 @@ paddleCollision game = leftPaddleCollision || rightPaddleCollision
       x - ballRadius <= rightPaddleX &&
       y + ballRadius >= player2 game - paddleHeight &&
       y - ballRadius <= player2 game + paddleHeight
-    
-wallBounce :: PongGame -> PongGame
-wallBounce game = game { ballVeloc = (vx, vy') }
-  where 
-    (vx, vy) = ballVeloc game
+  in
+    leftPaddleCollision || rightPaddleCollision
 
-    vy' = if wallCollision (ballLoc game) ballRadius
-          then
-            -vy
-          else
-            vy
+wallBounce :: PongGame -> PongGame
+wallBounce game =
+  let
+    (vx, vy) = ballVeloc game
+    vy' = 
+      if wallCollision (ballLoc game) ballRadius
+      then -vy
+      else vy
+  in
+    game { ballVeloc = (vx, vy') }
 
 wallCollision :: Position -> Radius -> Bool
-wallCollision (_, y) radius = topCollision || bottomCollision
-  where
-    topCollision = y - radius <= -fromIntegral width / 2
-    bottomCollision = y + radius >= fromIntegral width / 2
+wallCollision (_, y) radius =
+    let topCollision = y - radius <= -fromIntegral width / 2
+        bottomCollision = y + radius >= fromIntegral width / 2
+    in topCollision || bottomCollision
 
 handleKeys :: Event -> PongGame -> PongGame
 handleKeys (EventKey (Char 'r') _ _ _) game =
@@ -161,57 +185,50 @@ handleKeys (EventKey (Char 'r') _ _ _) game =
 handleKeys (EventKey (Char 'p') Down _ _) game =
   game { paused = not (paused game) }
 handleKeys (EventKey (Char 'w') keyState _ _) game =
-  case keyState of
-    Down -> game { wKeyPressed = True } 
-    Up -> game { wKeyPressed = False } 
+  updateControl game WKey (keyState == Down)
 handleKeys (EventKey (Char 's') keyState _ _) game =
-  case keyState of
-    Down -> game { sKeyPressed = True } 
-    Up -> game { sKeyPressed = False } 
+  updateControl game SKey (keyState == Down)
 handleKeys (EventKey (SpecialKey KeyUp) keyState _ _) game =
-  case keyState of
-    Down -> game { arrUpKeyPressed = True } 
-    Up -> game { arrUpKeyPressed = False } 
+  updateControl game ArrUpKey (keyState == Down)
 handleKeys (EventKey (SpecialKey KeyDown) keyState _ _) game =
-  case keyState of
-    Down -> game { arrDownKeyPressed = True } 
-    Up -> game { arrDownKeyPressed = False } 
-
+  updateControl game ArrDownKey (keyState == Down)
 handleKeys _ game = game
 
-window :: Display
-window = InWindow "Pong" (width, height) (0, 0)
-
-background :: Color
-background = black
-
-fps :: Int
-fps = 120
-
 updatePaddles :: PongGame -> PongGame
-updatePaddles game = game { player1 = newPlayer1Pos, player2 = newPlayer2Pos }
-  where
-    newPlayer1Pos
-      | wKeyPressed game && player1 game + 2 <= fromIntegral (height `div` 2) - (paddleHeight / 2) - bottomTopLineThickness = player1 game + 2
-      | sKeyPressed game && player1 game - 2 >= -fromIntegral (height `div` 2) + (paddleHeight / 2) + bottomTopLineThickness = player1 game - 2
-      | otherwise = player1 game
-
-    newPlayer2Pos
-      | arrUpKeyPressed game && player2 game + 2 <= fromIntegral (height `div` 2) - (paddleHeight / 2) - bottomTopLineThickness = player2 game + 2
-      | arrDownKeyPressed game && player2 game - 2 >= -fromIntegral (height `div` 2) + (paddleHeight / 2) + bottomTopLineThickness = player2 game - 2
-      | otherwise = player2 game
-
+updatePaddles game =
+  let
+    paddleTopLimit = fromIntegral (height `div` 2) - (paddleHeight / 2) - bottomTopLineThickness
+    paddleBottomLimit = -fromIntegral (height `div` 2) + (paddleHeight / 2) + bottomTopLineThickness
+    
+    moveUp currentPos = currentPos + 2
+    moveDown currentPos = currentPos - 2
+    
+    newPlayer1Pos =
+      if isKeyPressed game WKey && moveUp (player1 game) <= paddleTopLimit
+        then moveUp (player1 game)
+      else if isKeyPressed game SKey && moveDown (player1 game) >= paddleBottomLimit
+        then moveDown (player1 game)
+      else player1 game
+    
+    newPlayer2Pos =
+      if isKeyPressed game ArrUpKey && moveUp (player2 game) <= paddleTopLimit
+        then moveUp (player2 game)
+      else if isKeyPressed game ArrDownKey && moveDown (player2 game) >= paddleBottomLimit
+        then moveDown (player2 game)
+      else player2 game
+  in
+    game { player1 = newPlayer1Pos, player2 = newPlayer2Pos }
 
 -- if ballLoc (N, m) passed width or height, reset ballLoc and set game to paused
 gameFinished :: PongGame -> PongGame 
-gameFinished game = if abs (fst (ballLoc game)) >= fromIntegral width / 2
-                    then game { ballLoc = (0, 0), player1 = player1InitPosition, player2 = player2InitPosition, paused = True }
-                    else game
+gameFinished game = 
+  if abs (fst (ballLoc game)) >= fromIntegral width / 2
+  then game { ballLoc = (0, 0), player1 = player1InitPosition, player2 = player2InitPosition, paused = True }
+  else game
 
-
-update :: Float -> PongGame -> PongGame
 -- function composition operator
 -- on update, check if game is paused. If it is, return same pong game
+update :: Float -> PongGame -> PongGame
 update seconds game 
   | paused game = game
   | otherwise = updatePaddles . gameFinished  . paddleBounce . wallBounce . moveBall seconds $ game

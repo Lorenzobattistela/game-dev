b23213fd4757cfe18e55e2846dea11db9ddadbda
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-07-17 10:47:31 -0300

    adding more rendering helpers

--- survival/src/Rendering.hs
+++ survival/src/Rendering.hs
@@ -2,11 +2,15 @@ module Rendering where
 
 import qualified SDL
 import qualified SDL.Image
-import Control.Monad (forM_)
+import qualified SDL.Font as TTF
+import qualified SDL.Video.Renderer as Renderer
+import Control.Monad (forM_, when)
+import Control.Exception (bracket)
 import Foreign.C.String (withCString)
 import Foreign.C.Types (CInt)
 import Data.Vector (Vector)
 import qualified Data.Vector as V
+import Game
 
 
 -- since were receiving everything as args no need to a loadEnemySpritesheet fn as in C
@@ -43,9 +47,47 @@ renderCharacter renderer (spritesheet, spriteClips) x y currentFrame spriteWidth
     SDL.copy renderer spritesheet (Just sourceRect) (Just renderQuad)
 
 
+renderEnemy :: SDL.Renderer -> (SDL.Texture, Vector (SDL.Rectangle CInt)) 
+               -> Enemy -> Int -> Int -> IO ()
+renderEnemy renderer (spritesheet, enemyClips) enemy enemyWidth enemyHeight = do
+  let renderQuad = SDL.Rectangle
+          (SDL.P (SDL.V2 ( round $ x (ePosition enemy)) (round $ y (ePosition enemy))))
+          (SDL.V2 (fromIntegral enemyWidth) (fromIntegral enemyHeight))
+  
+  let sourceRect = enemyClips V.! (fromEnum (currentFrame enemy))
+  SDL.copy renderer spritesheet (Just sourceRect) (Just renderQuad)
 
 
+-- render all enemies if enemy is not dead. pass enemyWidth = 36 and enemyHeight = 36 to renderEnemy
+renderEnemies :: SDL.Renderer -> (SDL.Texture, Vector (SDL.Rectangle CInt)) -> [Enemy] -> IO ()
+renderEnemies renderer enemySpritesheet enemies = do
+  forM_ enemies $ \enemy ->
+    when (not $ eDead enemy) $
+      renderEnemy renderer enemySpritesheet enemy 36 36
 
+renderFloor :: SDL.Renderer -> FilePath -> IO ()
+renderFloor renderer path = do
+  let tileWidth = 45
+      tileHeight = 20
+      windowWidth = 950
+      windowHeight = 800
+
+  surface <- SDL.Image.load path
+  texture <- SDL.createTextureFromSurface renderer surface
+  SDL.freeSurface surface
+
+  let srcRect = SDL.Rectangle (SDL.P (SDL.V2 16 200)) (SDL.V2 45 20)  
+      windowSize = SDL.V2 windowWidth windowHeight
+      tilesX = (windowWidth + tileWidth - 1) `div` tileWidth
+      tilesY = (windowHeight + tileHeight - 1) `div` tileHeight
+
+  forM_ [0..tilesY-1] $ \y ->
+    forM_ [0..tilesX-1] $ \x -> do
+      let dstRect = SDL.Rectangle
+              (SDL.P (SDL.V2 (fromIntegral (x * tileWidth)) (fromIntegral (y * tileHeight))))
+              (SDL.V2 (fromIntegral tileWidth) (fromIntegral tileHeight))
+      SDL.copy renderer texture (Just srcRect) (Just dstRect)
+  SDL.destroyTexture texture
 
 
 
